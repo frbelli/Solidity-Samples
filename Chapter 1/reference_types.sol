810fe011e3bfa9574bed413d33fd00b8f5eed8f6
frbelli <frbelli@gmail.com>
2020-06-01 18:04:48 +0200

    Example of reference mapping

---
+++ Chapter 1/reference_types.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.1;
+contract EmrPatients{
+  // This is the Constructor and it gets called ONCE only when contract is first deployed
+  constructor() public{
+    // Do Nothing For now
+  }
+  // Define mapping to store key-value pairs
+  mapping(string=>medicalRecord)
+  patientRecordMapping;
+
+  // Organize medical records using struct
+  struct medicalRecord
+  {
+    string report_name;
+    address owner;
+  }
+  // Save medical record
+  function saveMedicalReport(string memory mr_number, string memory report_name, address p_addr) public payable {
+    patientRecordMapping[mr_number] = medicalRecord(report_name, p_addr);
+  }
+
+  function getReportByMedicalRecordNumber(string memory mr_number) public view returns (string memory, address) {
+    return (patientRecordMapping[mr_number].report_name, patientRecordMapping[mr_number].owner);
+  }
+}
